7a64d52219c0d7a5fcfca7933192341f3056a476
PurviMurkute <purvimurkute51@gmail.com>
2026-07-03 12:52:41 +0530

    Learned contructors in class and object

--- ClassObject.py
+++ ClassObject.py
@@ -6,3 +6,17 @@ class Student:
 #Creating Object (Instance)
 S1 = Student()
 print(S1.name)
+
+# __init__() function : Constructor
+
+class Students:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+# Creating Object (Instance)
+S1 = Students("John", 20)
+print(S1.name, S1.age)
+
+S2 = Students("Alice", 22)
+print(S2.name, S2.age)
